9e81ebd9be051722ec5f441f1ea4a4487430c232
mayowasamuel <fpasamuelmayowa51@gmail.com>
2025-08-29 08:55:20 +0100

    http://localhost:5173/

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.all('*', (req, res, next) => {
 
 app.use((req, res, next) => {
     res.setHeader('Content-Type', 'application/json');
-    res.setHeader('Access-Content-Allow-Orgin', 'https://www.minatofoundation.org/')
+    res.setHeader('Access-Content-Allow-Orgin', 'https://www.minatofoundation.org/', 'http://localhost:5173/')
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH,DELTE')
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
     response.head("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
