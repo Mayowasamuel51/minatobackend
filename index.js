6b6178e4e5c3f7154a404be121a217b966475e96
mayowasamuel <fpasamuelmayowa51@gmail.com>
2025-08-31 08:26:48 +0100

    https://minatoadminadminpage.vercel.app/

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const app = require('./api/index')
 const cors = require("cors");
 
 
-// app.use(cors({
-//     origin: ['http://localhost:5173', 'http://localhost:3000','https://minatoadminadminpage.vercel.app'], // allowed origins
-//     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
-//     allowedHeaders: ['Content-Type', 'Authorization']
-// }));
+app.use(cors({
+    origin: ['http://localhost:5173', 'http://localhost:3000', 'https://minatoadminadminpage.vercel.app'],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
+}));
+
 
 // ✅ Preflight handling
 // app.options('*', cors());
